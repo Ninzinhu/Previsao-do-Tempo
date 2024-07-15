5c453e5588feffeeb875190267337ecec96b94df
João <pel118201@gmail.com>
2024-07-15 18:10:42 -0300

    build: add entry

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './path/to/my/entry/file.js',
+  entry: './scritps/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'my-first-webpack.bundle.js',
