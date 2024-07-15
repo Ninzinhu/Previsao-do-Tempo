d9c11337cb97b9c99ef1fab01df9d431a04edea9
João <pel118201@gmail.com>
2024-07-14 23:29:23 -0300

    refactor: API loc

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -4,6 +4,9 @@ document.getElementById('loc').addEventListener('click', function() {
    // Chamar a API do tempo com a localização
    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=b187af6e8555c121214527c3c808a72e
 &lang=pt_br`)
+.then(response => response.json())
+.then(data => {
+  const weather = data.weather[0].main.toLowerCase();
+  const body = document.body;
 
-
-})
+});
