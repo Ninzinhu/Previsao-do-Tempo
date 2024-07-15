58715dd2679114d42383ca021441442230515014
João <pel118201@gmail.com>
2024-07-14 23:28:12 -0300

    feat: add get API

---
+++ public/scripts/index.js
@@ -0,0 +1,9 @@
+document.getElementById('loc').addEventListener('click', function() {
+  const location = document.getElementById('input-loc').value;
+  
+   // Chamar a API do tempo com a localização
+   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=b187af6e8555c121214527c3c808a72e
+&lang=pt_br`)
+
+
+})
